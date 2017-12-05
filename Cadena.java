b6dc963a4fbfa76ad71472b537bba3fb420dfda9
Joaquin <[joaquin.diaz.ramirez.8897@gmail.com]>
2017-12-05 11:36:37 +0100

    Ejercicio 2

---
+++ Cadena.java
@@ -0,0 +1,12 @@
+import java.lang.String;
+
+public class Cadena{
+	
+	String str = "JoaquínDíazRamírez";
+	public static void main(String [] args){
+		
+		System.out.println(" El conjunto de palabras a trasnformar son:");
+		System.out.println("JoaquínDíazRamírez");
+		
+	}
+}
